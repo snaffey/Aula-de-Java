90685a6e98c696b267496f3e669ccdf10fa928c6
Tiago <68923057+snaffey@users.noreply.github.com>
2022-04-29 15:27:29 +0100

    Criação da aula 8 e codigo em js

--- js/aula8.js
+++ js/aula8.js
@@ -1,8 +1,11 @@
 window.onload = function(){
-    var nome = "Sergio";
-    gravarCookie("nomeCookie", nome, 15);
-    lerCookie("nomeCookie");
-    var res = lerCookie("nomeCookie");
-    alert(res);
-    eliminarCookie("nomeCookie")
+    var introP = document.getElementById("intro");
+    //alert(introP);
+    var intro2 = document.getElementsByClassName("intro2"); //coleção de nos
+    //alert(intro2);
+    var h3 = document.getElementsByTagName("h3");
+    //alert(h3);
+    //Manipulação do valor de um objeto
+    introP.innerHTML = "Alterado via API DOM";
+    introP.style.color = "#ddff34"
 }
