c52f08e74d69ed5a196f240989993b03ebff2713
Tiago <ggtiago0santos@gmail.com>
2022-05-20 16:14:15 +0100

    terminar aula2

--- jquery/js/aula2.js
+++ jquery/js/aula2.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-	alert("teste");
-})
+	// Seletores
+	/*
+	Seletores Simples - usados para aceder o elemento
+	Seletor Universal - * , todos recebem este comportamento
+	*/
+	// $("*").css("color","#0000ff");
+
+	//Seletor identificador - #nome do identificador
+	//$(#nome).css("color","#ff00ff");
+
+	//Seletor grupo de class - .nome da classe
+	//$(".p2").css("color","#cc001f");
+
+	//Seletor elemento tag html
+	//$("span").css("color","#0000ff")
+
+	//Seletor grupo
+	//$(".p2, span").css("color","#0000ff");
+
+	//Seletores compostos (hierarquia)
+	//$("p span").css("color","#0000ff");
+
+	//Descendente filho direto
+	$("p > span").css("color","#0000ff")
+});
