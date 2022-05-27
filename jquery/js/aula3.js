6363b309a6133ce8db943f98c1fcae68f37d2a10
Tiago <ggtiago0santos@gmail.com>
2022-05-27 15:11:31 +0100

    a

--- jquery/js/aula3.js
+++ jquery/js/aula3.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
     //Seletores de controlo de formularios
-    
+    //Sintaxe :input - recupera qualquer tipo de elemento que seja input
+    //$(":input").css("background","#cd00cd");
+    //$(":input").css("display","block");
+    //$(":text").css("background","#cd00cd")
+
+    /*
+    chackboxm radio ->
+    Este tipo de input tem metodos atrelados, que permitem percorrer os elementos next(), prev(), parent()
+    */
+
+    $(":button").click(function(){
+        //$(":text").css("background","#cd00cd");
+        //$(":password").css("background","#cd00cd");
+        $(":radio").parent().css("background","#cd00cd")
+    }).css("border","2px solid #ff00cd",);
 });
