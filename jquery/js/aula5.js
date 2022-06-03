08dd2818e1072a40b30d35f05c6fa8b82a263fe6
Tiago <ggtiago0santos@gmail.com>
2022-06-03 16:19:55 +0100

    fim de aula 3 jun

--- jquery/js/aula5.js
+++ jquery/js/aula5.js
@@ -29,8 +29,36 @@ $(document).ready(function(){
         var teste1 = $("img").hasClass("img");
         //alert(teste1);
 
-        $("img").removeClass("img");
+        //$("img").removeClass("img");
         $("img").removeAttr("style");
         $("img").addClass("imgClass");
+        /*
+        A diferença entre o offset é relativo ao documento,
+        o position é relativo ao ancentral sobre o qual o elemento posicionado.
+        */
+        /*
+        var pt = $("img").position().top;
+        var pl = $("img").position().left;
+        alert(pt);
+        alert(pl);
+
+        var poft = $("img").offset().top;
+        var pofl = $("img").offset().left;
+        alert(poft);
+        alert(pofl);
+        */
+        //fim do click
+
+        $("#cab1").text("Fui clicado");
+
+    }).dblclick(function(){
+        $("#cab1").text("Fui clicado em dblclick");
+    }).mouseover(function(){
+        $("#cab1").text("Sai de cima animal").css("color","#ff00cd");
+    }).mouseout(function(){
+        $("#cab1").text("Saiste seu animal").css("color","#ff34ff");
+    }).mousemove(function(posicao){
+        //clientX, clientY -> posição do rato em px
+        $("#cab1").text("PosX ->"+posicao.clientX+" PosY ->"+posicao.clientY).css("color","#000000");
     }).css("border","2px solid #ff00cd");
 });
