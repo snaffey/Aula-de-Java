53a0652133540f6c08f7f9cca90f690ecaeddfb9
Tiago <ggtiago0santos@gmail.com>
2022-06-03 15:28:49 +0100

    aula 5

---
+++ jquery/js/aula5.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("disJQ").click(function(){
+        /*
+        Elemento.addClass - adiciona uma class ao elemento
+        Elemento.position() - devolve a pos do elemento
+        Elemento.offset() - devolve a margem de impressão do elemento
+        */
+        var h = $("img").css("height");
+        //alert(h);
+        var w = $("img").css("width");
+        //alert(w);
+        var hImg = $("img").height();
+        //alert(hImg);
+
+        //Devolver a altura da imagem + o espaçamento aplicado
+        
+    }).css("border","2px solid #ff00cs");
+});
