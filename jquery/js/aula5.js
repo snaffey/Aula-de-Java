3e56d92ae2b979e0bbf54397520d996b801d6069
Tiago <ggtiago0santos@gmail.com>
2022-06-03 15:53:08 +0100

    quase no fim da aula 3 jun

--- jquery/js/aula5.js
+++ jquery/js/aula5.js
@@ -16,8 +16,21 @@ $(document).ready(function(){
         var h2Img = $("img").innerHeight();
         //alert(h2Img);
 
-        //Devolver a altura da imagem + o espaçamento
+        //Devolver a altura da imagem + o espaçamento aplicado + a borda do elemento
         var h3Img = $("img").outerHeight();
-        alert(h3Img);
+        //alert(h3Img);
+
+        //Devolver a altura da imagem + o espaçamento aplicado + a borda do elemento + margem
+        var h4Img = $("img").outerHeight(true);
+        //alert(h4Img);
+
+        var teste = $("img").hasClass();
+        //alert(teste);
+        var teste1 = $("img").hasClass("img");
+        //alert(teste1);
+
+        $("img").removeClass("img");
+        $("img").removeAttr("style");
+        $("img").addClass("imgClass");
     }).css("border","2px solid #ff00cd");
 });
